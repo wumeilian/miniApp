eaebf88189223740045614d282d849e9fa2400f3
wumeilian <1054527951@qq.com>
2019-03-23 19:15:08 +0800

    add style

--- pages/index/index.js
+++ pages/index/index.js
@@ -4,24 +4,28 @@ const app = getApp()
 
 Page({
   data: {
-    date: '2016-09-01',
+    date: ['2019', '01'],
     motto: 'Hello World',
     userInfo: {},
     hasUserInfo: false,
     canIUse: wx.canIUse('button.open-type.getUserInfo')
   },
+
+  bindDateChange(e) {
+    console.log('picker发送选择改变，携带值为', e.detail.value)
+
+    this.setData({
+      date: e.detail.value.split('-')
+    })
+  },
+
   //事件处理函数
   bindViewTap: function() {
     wx.navigateTo({
       url: '../logs/logs'
     })
   },
-  bindDateChange(e) {
-    console.log('picker发送选择改变，携带值为', e.detail.value)
-    this.setData({
-      date: e.detail.value
-    })
-  },
+  
   onLoad: function () {
     if (app.globalData.userInfo) {
       this.setData({
